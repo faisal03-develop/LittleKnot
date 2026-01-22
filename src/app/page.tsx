d2fd2e4dd88e12ba3002b0383ca7c5adfcd31f86
Faisal Altaf <altaffaisal532@gmail.com>
2026-01-22 14:57:51 +0500

    Added the SignUp Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-
 import Navbar from "@/components/navbar/index"
 import SignUpBanner from "@/components/sign-up-banner/index"
+
+
+
+
 export default function Home() {
   return (
-    <>
+    <div className="container mx-auto px-4">
     <SignUpBanner />
     <Navbar />
-    </>
+    </div>
   );
 }
--- src/components/sign-up-banner/index.tsx
+++ src/components/sign-up-banner/index.tsx
@@ -1,5 +1,11 @@
 export default function index() {
   return (
-    <div>Banner</div>
+    <div>
+        <div className="h-[34px] md:h-[38px] bg-black w-full flex justify-between items-center px-5">
+            <span className="text-black">x</span>
+            <span className="text-white">SignUp and get 20% of to your first order <a href="#" className="text-white underline">Sign Up</a></span>
+            <span className="text-white">x</span>
+        </div>
+    </div>
   )
 }
