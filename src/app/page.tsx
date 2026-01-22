4a93855215475083286a3cf4c0acc15a7f5a4a3a
Faisal Altaf <altaffaisal532@gmail.com>
2026-01-22 14:36:43 +0500

    Initial Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+
 import Navbar from "@/components/navbar/index"
+import SignUpBanner from "@/components/sign-up-banner/index"
 export default function Home() {
   return (
     <>
+    <SignUpBanner />
     <Navbar />
     </>
   );
