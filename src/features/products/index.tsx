30fe164d54e28ca9599d905b182e6291d9aa119b
Faisal Altaf <altaffaisal532@gmail.com>
2026-01-26 18:20:48 +0500

    Added top selling projects

--- src/features/products/index.tsx
+++ src/features/products/index.tsx
@@ -5,12 +5,12 @@ export default async function ProductsPage() {
   const products = await getProducts()
 
   return (
-    <div className="px-6 py-10">
+    <div className="mx-20 py-10">
       <h1 className="mb-10 text-3xl text-center font-extrabold">NEW ARRIVALS</h1>
       <ProductGrid products={products} limit={4} />
-      <div className=''>
-
-      </div>
+      <div className='bg-gray-300 h-0.5 my-20'></div>
+      <h1 className="mb-10 text-3xl text-center font-extrabold">TOP SELLING</h1>
+      <ProductGrid products={products} limit={4} />
     </div>
   )
 }
