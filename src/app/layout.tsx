a49c07b198b0864f17f40fe47048159b255d044f
Faisal Altaf <altaffaisal532@gmail.com>
2026-01-27 14:51:18 +0500

    Added Local Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+// import {Satoshi} from 'next/font/local/'
 import "./globals.css";
 import { Inter } from "next/font/google";  // Assuming you want Inter; if Geologica, import that instead
 
@@ -8,6 +9,37 @@ const interFont = Inter({
   subsets: ["latin"],
   weight: ["400", "700", "900"],
 });
+
+// const satoshiFont = Satoshi({
+//   variable: "--font-satoshi",
+//   src: [
+//     {
+//       path: '../assets/fonts/Satoshi-Regular.woff2',
+//       weight: '300',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../assets/fonts/Satoshi-Medium.woff2',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../assets/fonts/Satoshi-Black.woff2',
+//       weight: '600',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../assets/fonts/Satoshi-Light.woff2',
+//       weight: '200',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../assets/fonts/Satoshi-Bold.woff2',
+//       weight: '500',
+//       style: 'normal',
+//     },
+//   ]
+// })
 
 export const metadata: Metadata = {
   title: "Little Knot",
@@ -22,7 +54,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${interFont.variable} antialiased`}
+        className={`${interFont.variable} --font-inter antialiased`}
       >
         {children}
       </body>
