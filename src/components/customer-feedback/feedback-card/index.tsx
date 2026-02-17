fe46d282c127a1c2826d70d12b8b248b27777cea
Faisal Altaf <altaffaisal532@gmail.com>
2026-02-17 15:24:20 +0500

    FIxed ClientFeedback Width

--- src/components/customer-feedback/feedback-card/index.tsx
+++ src/components/customer-feedback/feedback-card/index.tsx
@@ -16,7 +16,7 @@ export default function FeedBackCard({
 
   return (
     <div>
-      <div className="flex flex-col max-w-96 rounded-2xl border-gray-300 border-2 p-4 items-center md:items-start hover:scale-105 transition-all duration-300 p-7 w-auto">
+      <div className="flex flex-col rounded-2xl border-gray-300 border-2 p-4 items-center md:items-start hover:scale-105 transition-all duration-300 p-7">
         <RatingStar rating={Math.ceil(rating)} />
         <div className="flex items-center mt-4">
           <span className="font-semibold text-xl">{name.split(" ")[0]}</span>
